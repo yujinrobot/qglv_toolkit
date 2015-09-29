30ad04896a595e90372e5b51fab97d031a72fcfb
Daniel Stonier <d.stonier@gmail.com>
2015-09-29 16:29:58 +0900

    [qglv_extensions] ported from dslam viewer.

---
+++ qglv_extensions/include/qglv/ext/sophus.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file include/qglv/sophus2qgl.hpp
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef qglv_extensions_SOPHUS_HPP_
+#define qglv_extensions_SOPHUS_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QGLViewer/frame.h>
+#include <sophus/se3.hpp>
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace qglv {
+namespace ext {
+
+/**
+ * Convert a sophus pose to a qglviewer frame.
+ */
+qglviewer::Frame sophusPose2QGLFrame(const Sophus::SE3f& pose);
+
+/**
+ * Get the translational part of a sophus pose as a qglviewer Vec.
+ * @param pose
+ * @return
+ */
+qglviewer::Vec sophusPose2QGLTranslation(const Sophus::SE3f& pose);
+
+qglviewer::Quaternion sophusPose2QGLOrientation(const Sophus::SE3f& pose);
+
+} // namespace ext
+} // namespace qglv
+
+#endif /* qglv_extensions_SOPHUS_HPP_ */
